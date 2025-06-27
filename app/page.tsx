4b0b1fa51128ba29310975e04271d8e8c99c2ed9
rashaan <rmng.developer@gmail.com>
2025-06-27 13:31:56 -0400

    setting up basic design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import Storywriter from "@/components/Storywriter";
+
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main className="flex-1 flex flex-col">
+      {/* UI component */}
+      <section className="flex-1 grid grid-cols-1 lg:grid-cols-2">
+        <div className="bg-purple-400 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pb-10">
+          {/* insert logo here later with Image imported from next/image */}
+          <img
+            src="https://freepngimg.com/save/40968-fairytale-castle-free-clipart-hd/565x565"
+            alt="castle"
+            height={250}
+            width={250}
+          />
+          <Button
+            asChild
+            className="px-20 bg-purple-700 p-10 text-xl font-bold"
+          >
+            <Link href="/stories">Explore Story Library</Link>
+          </Button>
+        </div>
+
+        {/* story writer  */}
+
+        <div>
+          <Storywriter />
+        </div>
+      </section>
+    </main>
+  );
 }
---
+++ components/Storywriter.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState } from "react";
+import { Textarea } from "./ui/textarea";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "./ui/select";
+import { Button } from "./ui/button";
+
+function Storywriter() {
+  return (
+    <div className="flex flex-col container">
+      <section className="flex-1 flex flex-col border border-purple-800 rounded-md p-10 space-y-2">
+        <Textarea placeholder="Write a story about a robot and a human who become friends..." />
+        <Select>
+          <SelectTrigger>
+            <SelectValue placeholder="How many pages should the story be?" />
+          </SelectTrigger>
+          <SelectContent className="w-full">
+            {Array.from({ length: 10 }, (_, i) => {
+              return (
+                <SelectItem value={String(i + 1)} key={i}>
+                  {i + 1}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
+
+        <Button className="w-full">Generate Story</Button>
+      </section>
+      <section className="flex-1 pb-5 mt-5 bg-pink-200"></section>
+    </div>
+  );
+}
+export default Storywriter;
