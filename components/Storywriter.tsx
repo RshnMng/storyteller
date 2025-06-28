8a8037ab929c3d7ad7dde5196ece67db4415c12e
rashaan <rmng.developer@gmail.com>
2025-06-28 09:01:50 -0400

    finished honepage ui

--- components/Storywriter.tsx
+++ components/Storywriter.tsx
@@ -12,10 +12,14 @@ import { Button } from "./ui/button";
 
 function Storywriter() {
   const [storyInput, setStoryInput] = useState<string>("");
-  const [storyPages, setStoryPage] = useState<number>();
+  const [storyPages, setStoryPages] = useState<number>();
+  const [progress, setProgress] = useState<string>("");
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
+  const [currentTool, setCurrentTool] = useState("");
 
   return (
-    <div className="flex flex-col container">
+    <div className="flex flex-col container mx-auto">
       <section className="flex-1 flex flex-col rounded-md p-10 space-y-2">
         <Textarea
           onChange={(e) => setStoryInput(e.target.value)}
@@ -23,7 +27,7 @@ function Storywriter() {
           className="flex-1 text-black"
           placeholder="Write a story about a robot and a human who become friends..."
         />
-        <Select>
+        <Select onValueChange={(value) => setStoryPages(parseInt(value))}>
           <SelectTrigger>
             <SelectValue placeholder="How many pages should the story be?" />
           </SelectTrigger>
@@ -38,11 +42,42 @@ function Storywriter() {
           </SelectContent>
         </Select>
 
-        <Button className="w-full" size="lg">
+        <Button
+          disabled={!storyInput || !storyPages}
+          className="w-full"
+          size="lg"
+        >
           Generate Story
         </Button>
       </section>
-      <section className="flex-1 pb-5 mt-5 bg-pink-200"></section>
+      <section className="flex-1 pb-5 mt-5 bg-pink-200">
+        <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 everflow-y-scroll">
+          <div>
+            {runFinished === null && (
+              <>
+                <p className="animate-pulse">
+                  Im waiting for you to Generate a story...
+                </p>{" "}
+              </>
+            )}
+            <span className="mr-5">{">>"}</span>
+            {progress}
+          </div>
+
+          {currentTool && (
+            <div className="py-10">
+              <span className="mr-5">{`--- [Current Tool] --- `}</span>
+              {currentTool}
+            </div>
+          )}
+
+          {runStarted && (
+            <div>
+              <span className="mr-5 animate-in">{`--- [AI Storyteller Has Started!] --- `}</span>
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
